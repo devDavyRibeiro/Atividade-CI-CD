a597f5f18575738c48a0178c6417db5805e0e71f
Davy Ribeiro <davy.oliv.ribeiro@gmail.com>
2025-09-19 21:34:14 -0300

    fix:corrigindo testes

--- tests/server.test.js
+++ tests/server.test.js
@@ -10,12 +10,12 @@ request.describe("Items API", () => {
     request.expect(res.body.ok).toBe(true);
   });
 
-  //it("POST /items cria item", async () => {
-  //  const res = await request(app).post("/items").send({ name: "caderno", quantity: 2 });
-  //  expect(res.status).toBe(201);
-  //  expect(res.body).toMatchObject({ name: "caderno", quantity: 2 });
-  //  expect(res.body.id).toBeDefined();
-  //});
+  it("POST /items cria item", async () => {
+   const res = await request(app).post("/items").send({ name: "caderno", quantity: 2 });
+   expect(res.status).toBe(201);
+   expect(res.body).toMatchObject({ name: "caderno", quantity: 2 });
+   expect(res.body.id).toBeDefined();
+  });
 
   request.it("GET /items lista ao menos 1", async () => {
     const res = await request(app).get("/items");
